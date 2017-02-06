d8a4b4b8108403e9978b17623a4162c2b7dde2f1
Raphael Nachabr <raphael.nachbar@navegarti.com.br>
2017-02-06 13:13:36 -0200

    javaScript - aula 3

--- cursos/javaScript/javaScript-basico/projeto/calcula-imc.js
+++ cursos/javaScript/javaScript-basico/projeto/calcula-imc.js
@@ -1,13 +1,36 @@
+// DOM - Document Object Model, é uma representação de uma página html no mundo Javascript e pode ser acessado a partir da variável document, que representa a raiz de nossa página
+// textContent pega conteudo, altera conteudo de um elemento
+// var titulo = document.getElementById("titulo"); // Seleciona elemento
+// var conteudo = titulo.textContent; // Pega texto dentro do elemento
+// console.log(conteudo); // Imprime o conteudo
+// var tdDoImc = document.getElementById("imc-1"); // pega o td do imc do paciente 1
+// tdDoImc.textContent = imc; //Altera o conteúdo do td para o valor da variável imc
+
 // peso / altura * altura
 
-var peso = 100;
-var altura = 2.0;
+// retorna o objeto que representa o id - manipulando DOM
+var tdPeso = document.getElementById("peso-2"); 
+var tdAltura = document.getElementById("altura-2");
+
+// Um objeto em Javascript, nada mais é que uma variável que possui, dentro dela, chaves e valores
+// objeto, chave : valor
+var paciente = {
+	peso : tdPeso.textContent,
+	altura : tdAltura.textContent 
+};
+// paciente["altura"]; // pega valor do objeto
+// dot notation para pegar valores de um objeto - mais usado
+// paciente.peso; 
+// paciente.altura; 
 
 // var ehDiferenteDeZero = (altura != 0); // varivel de comparação, tipo boolean (true ou false)
-
-if (altura != 0) {
+if (paciente.altura != 0) {
 	// js executa as contas da esquerda para a direita
-	var imc = peso / (altura * altura); 
+	var imc = paciente.peso / (paciente.altura * paciente.altura); 
+
+	var tdImc = document.getElementById("imc-2");
+	tdImc.textContent = imc; // modificando o valor de um elemento
+
 	console.log(imc);
 } else {
 	console.log("Não executei por que altura eh igual a zero");
