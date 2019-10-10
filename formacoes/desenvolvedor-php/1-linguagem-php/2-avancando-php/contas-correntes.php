28d82da62841d28a63028962e249f5686c9c6f66
Raphael Nachbar <raphael.nachbar@navegarti.com.br>
2019-10-10 11:41:29 -0300

    Formação Desenvolvedor PHP

---
+++ formacoes/desenvolvedor-php/1-linguagem-php/2-avancando-php/contas-correntes.php
@@ -0,0 +1,24 @@
+<?php
+
+// Arrays Associativos
+
+$conta1 = [
+  'titular' => 'Raphael',
+  'saldo' => 1000
+];
+
+$conta2 = [
+  'titular' => 'Maria',
+  'saldo' => 10000
+];
+
+$conta3 = [
+  'titular' => 'João',
+  'saldo' => 300
+];
+
+$contasCorrentes = [$conta1, $conta2, $conta3];
+
+for ($i = 0; $i < count($contasCorrentes) ; $i++) :
+  echo $contasCorrentes[$i]['titular'] . PHP_EOL;
+endfor;
---
+++ formacoes/desenvolvedor-php/1-linguagem-php/2-avancando-php/listas.php
@@ -0,0 +1,6 @@
+<?php
+
+$idadeList = [21, 23, 25, 19, 30, 27];
+$umaIdade = $idadeList[2];
+
+echo $umaIdade;
