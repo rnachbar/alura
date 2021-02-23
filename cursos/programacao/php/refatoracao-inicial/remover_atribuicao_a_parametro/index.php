313661ef156fc75889a4bedb49fbdf9ef855d2b0
Raphael Nachbar <raphael.nachbar@navegarti.com.br>
2021-02-23 14:33:28 -0300

    Remover atribuição a parâmetro

--- cursos/programacao/php/refatoracao-inicial/remover_atribuicao_a_parametro/index.php
+++ cursos/programacao/php/refatoracao-inicial/remover_atribuicao_a_parametro/index.php
@@ -1,29 +1,30 @@
 <?php declare(strict_types=1);
 
-function recebeDesconto(int $descontoInicial, bool $ehPremium, int $quantidade, int $anosCliente): void
-{
+function recebeDesconto(int $descontoInicial, bool $ehPremium, int $quantidade, int $anosCliente): void {
+    $desconto = $descontoInicial;
 
-    if ($descontoInicial > 200) {
+    if ($desconto > 200) {
         return;
     }
 
     if ($ehPremium === true) {
-        $descontoInicial = $descontoInicial * 1.1;
+        $desconto = $descontoInicial * 1.1;
     }
 
     if ($quantidade > 50) {
-        $descontoInicial = $descontoInicial * 1.2;
+        $desconto = $desconto * 1.2;
     }
 
     if ($anosCliente > 3) {
-        $descontoInicial = $descontoInicial * 1.1;
+        $desconto = $desconto * 1.1;
     }
 
     echo <<<EOF
---------------------
-Desconto: $descontoInicial
---------------------
-EOF;
+    --------------------
+    Desconto Inicial: $descontoInicial
+    Desconto: $desconto
+    --------------------
+    EOF;
 }
 
 recebeDesconto(50, true, 100, 100);
