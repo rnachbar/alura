8b279713a322d22b9b8ec61018b1ca157179515b
Raphael Nachbar <raphael.nachbar@navegarti.com.br>
2021-02-23 14:18:28 -0300

    Extrair variável

--- cursos/programacao/php/refatoracao-inicial/extrair_variavel/index.php
+++ cursos/programacao/php/refatoracao-inicial/extrair_variavel/index.php
@@ -1,8 +1,13 @@
 <?php declare(strict_types=1);
 
-function verificaSeEmailEhValido(): string
-{
-    return filter_var($_GET['email'], FILTER_VALIDATE_EMAIL) ? "O seu e-mail é: " . $_GET['email'] : "O seu e-mail não é válido";
+function verificaSeEmailEhValido(): string {
+    $email = $_GET['email'];
+    $sucesso = "O seu e-mail é: $email";
+    $erro = 'O seu e-mail não é válido';
+
+    return filter_var($email, FILTER_VALIDATE_EMAIL) 
+        ? $sucesso
+        : $erro;
 }
 
 echo verificaSeEmailEhValido();
