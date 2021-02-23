5c15e3987bd3166f142258b849fdfae2e5c44fcc
Raphael Nachbar <raphael.nachbar@navegarti.com.br>
2021-02-23 14:47:13 -0300

    Mover método

--- cursos/programacao/php/refatoracao-inicial/mover_metodo/ContaCorrente.php
+++ cursos/programacao/php/refatoracao-inicial/mover_metodo/ContaCorrente.php
@@ -2,19 +2,18 @@
 
 namespace Alura\MoverMetodo;
 
-class ContaCorrente
-{
+class ContaCorrente {
     private $correntista;
 
-    public function __construct(Correntista $correntista)
-    {
+    public function __construct(Correntista $correntista) {
         $this->correntista = $correntista;
     }
 
-    public function exibeNomeCorrentista(): string
-    {
-        $nome = $this->correntista->getNome();
-        $sobrenome = $this->correntista->getSobrenome();
-        return $nome . " " . $sobrenome;
+    public function exibeNomeCorrentista(): string {
+        // Tell, don't ask
+        // $nome = $this->correntista->getNome();
+        // $sobrenome = $this->correntista->getSobrenome();
+
+        return $this->correntista->getNomeCompleto();
     }
 }
--- cursos/programacao/php/refatoracao-inicial/mover_metodo/Correntista.php
+++ cursos/programacao/php/refatoracao-inicial/mover_metodo/Correntista.php
@@ -2,24 +2,24 @@
 
 namespace Alura\MoverMetodo;
 
-class Correntista
-{
+class Correntista {
     private $nome;
     private $sobrenome;
 
-    public function __construct(string $nome, string $sobrenome)
-    {
+    public function __construct(string $nome, string $sobrenome) {
         $this->nome = $nome;
         $this->sobrenome = $sobrenome;
     }
 
-    public function getNome(): string
-    {
+    public function getNome(): string {
         return $this->nome;
     }
 
-    public function getSobrenome(): string
-    {
+    public function getSobrenome(): string {
         return $this->sobrenome;
+    }
+
+    public function getNomeCompleto(): string {
+        return $this->nome . " " . $this->sobrenome;
     }
 }
