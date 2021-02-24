e44b98bef5ff79c97a4ee34d9f9e19c5e657cb11
Raphael Nachbar <raphael.nachbar@navegarti.com.br>
2021-02-24 10:36:44 -0300

    Remover intermediário

--- cursos/programacao/php/refatoracao-inicial/remover_intermediario/index.php
+++ cursos/programacao/php/refatoracao-inicial/remover_intermediario/index.php
@@ -3,9 +3,8 @@
 namespace Alura\RemoverIntermediario;
 
 require 'Funcionario.php';
-require 'Empregado.php';
 
-$maria = new Empregado(new Funcionario('Giovanni', 'Instrutor'));
+$maria = new Funcionario('Giovanni', 'Instrutor');
 
 echo "<p>{$maria->getNome()}</p>";
 echo "<p>{$maria->getCargo()}</p>";
