9df167531353500e309252ab67bf047d9566835d
Raphael Nachbar <raphael.nachbar@navegarti.com.br>
2022-01-05 16:17:23 -0300

    Done: Aula 1

--- formacoes/desenvolvedor-php/1.linguagem-php/orientacao-a-objetos-2/src/Titular.php
+++ formacoes/desenvolvedor-php/1.linguagem-php/orientacao-a-objetos-2/src/Titular.php
@@ -1,32 +1,19 @@
 <?php
 
-class Titular
+class Titular extends Pessoa
 {
-    private $cpf;
-    private $nome;
+    private $endereco;
 
-    public function __construct(CPF $cpf, string $nome)
+    public function __construct(CPF $cpf, string $nome, Endereco $endereco)
     {
-        $this->cpf = $cpf;
-        $this->validaNomeTitular($nome);
-        $this->nome = $nome;
+        // Classe parent é clase de quem estou herdando, nesse caso o contrutor de Pessoa.
+        parent::__construct($nome, $cpf);
+
+        $this->endereco = $endereco;
     }
 
-    public function recuperaCpf(): string
+    public function recuperaEndereco(): Endereco
     {
-        return $this->cpf->recuperaNumero();
-    }
-
-    public function recuperaNome(): string
-    {
-        return $this->nome;
-    }
-
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 5) :
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        endif;
+        return $this->endereco;
     }
 }
