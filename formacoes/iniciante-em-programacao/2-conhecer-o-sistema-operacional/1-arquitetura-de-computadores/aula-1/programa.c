73295beeb5863af199a5bfc13866d6cca44e732b
Raphael Nachbar <raphael.nachbar@navegarti.com.br>
2022-01-23 13:40:29 -0300

    Done: Aula 1

---
+++ formacoes/iniciante-em-programacao/2-conhecer-o-sistema-operacional/1-arquitetura-de-computadores/aula-1/programa.c
@@ -0,0 +1,19 @@
+// COMPILADO
+#include <stdio.h>
+
+int main() {
+    int a = 7;
+    int b = 2;
+    int c = a + b;
+
+    if (c != 0) {
+        c++;
+    }
+
+    // for (int i = 0; i < 10000000; i++) {
+    //     c++;
+    // }
+
+    printf("Olá, c = %d\n", c);
+    return 0;
+}
---
+++ formacoes/iniciante-em-programacao/2-conhecer-o-sistema-operacional/1-arquitetura-de-computadores/aula-1/programa.py
@@ -0,0 +1,13 @@
+# INTERPRETADO
+
+a = 7
+b = 2
+c = a + b
+
+if c != 0:
+    c += 1
+
+# for i in range(10000000):
+#     c += 1
+
+print('Olá, c = ', c)
